cfe6ef468d86a5518b786a814b7a1fac9d95687c
mdshepard <mdshepard8@gmail.com>
2019-03-19 10:50:34 -0400

    Forms finished. Loads properly.

--- recipebox/admin.py
+++ recipebox/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import Author, Food, Unit, Recipe
+from .models import Author, Unit, Recipe  # Food
 
-admin.site.register(Food)
+admin.site.register(Recipe)
 admin.site.register(Author)
 admin.site.register(Unit)
-admin.site.register(Recipe)
+# admin.site.register(Food)
