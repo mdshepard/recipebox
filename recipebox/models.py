7c81e87d95fd2d1a08128228039129ab47aae273
mdshepard <mdshepard8@gmail.com>
2019-03-19 06:12:19 -0400

    initial commit.

---
+++ recipebox/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Author(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField()
+    name = models.CharField(max_length=40)
+
+    def __str__(self):
+        return self.name
+
+    def url(self):
+        return f"/author/{self.id}"
+
+
+class Recipe(models.Model):
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    title = models.CharField(max_length=120)
+    total_time = models.DurationField()
+    instructions = models.TextField(default='')
+    description = models.CharField(max_length=300, default='')
+
+    def __str__(self):
+        return self.title
+
+    def url(self):
+        return f"/recipe/{self.id}"
---
+++ recipebox/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+from recipebox.models import Author, Recipe
+
+
+def home_index(req):
+    recipes = Recipe.objects.all()
+    return render(req, 'index.html', {
+        'recipes': recipes,
+    }
+    )
+
+
+def author(req, author_id):
+    author = Author.objects.get(id=author_id)
+    recipes = Recipe.objects.filter(author=author)
+    return render(req, 'author.html', {
+        'author': author,
+        'recipes': recipes,
+    }
+    )
+
+
+def authors(req):
+    authors = Author.objects.all()
+    return render(req, 'authors.html', {
+        'authors': authors,
+    }
+    )
+
+
+def recipe(req, recipe_id):
+    recipe = Recipe.objects.get(id=recipe_id)
+    return render(req, 'recipe.html', {
+        'recipe': recipe
+    }
+    )
